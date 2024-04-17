31f18e999a049f7b4814e7efac0310ecebab007c
Curtis <curtis.stewart722@gmail.com>
2024-04-17 13:37:37 +0100

    Reformatted settings to use enums

--- code/settings.py
+++ code/settings.py
@@ -1,58 +1,86 @@
-import pygame
-from pygame.math import Vector2 as vector 
+from enum import Enum, auto
 from sys import exit
 
+import pygame
+from pygame.math import Vector2 as vector
+
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
-TILE_SIZE = 64 
+TILE_SIZE = 64
 ANIMATION_SPEED = 6
 BATTLE_OUTLINE_WIDTH = 4
 
-COLORS = {
-	'white': '#f4fefa', 
-	'pure white': '#ffffff',
-	'dark': '#2b292c',
-	'light': '#c8c8c8',
-	'gray': '#3a373b',
-	'gold': '#ffd700',
-	'light-gray': '#4b484d',
-	'fire':'#f8a060',
-	'water':'#50b0d8',
-	'plant': '#64a990', 
-	'black': '#000000', 
-	'red': '#f03131',
-	'blue': '#66d7ee'
-}
 
-WORLD_LAYERS = {
-	'water': 0,
-	'bg': 1,
-	'shadow': 2,
-	'main': 3,
-	'top': 4
-}
+class COLORS(Enum):
+    WHITE = "#f4fefa"
+    PURE_WHITE = "#ffffff"
+    DARK = "#2b292c"
+    LIGHT = "#c8c8c8"
+    GRAY = "#3a373b"
+    GOLD = "#ffd700"
+    LIGHT_GRAY = "#4b484d"
+    FIRE = "#f8a060"
+    WATER = "#50b0d8"
+    PLANT = "#64a990"
+    BLACK = "#000000"
+    RED = "#f03131"
+    BLUE = "#66d7ee"
 
-BATTLE_POSITIONS = {
-	'left': {'top': (360, 260), 'center': (190, 400), 'bottom': (410, 520)},
-	'right': {'top': (900, 260), 'center': (1110, 390), 'bottom': (900, 550)}
-}
 
-BATTLE_LAYERS =  {
-	'outline': 0,
-	'name': 1,
-	'monster': 2,
-	'effects': 3,
-	'overlay': 4
-}
+class WORLD_LAYER(Enum):
+    WATER = auto()
+    BG = auto()
+    SHADOW = auto()
+    MAIN = auto()
+    TOP = auto()
 
-BATTLE_CHOICES = {
-	'full': {
-		'fight':  {'pos' : vector(30, -60), 'icon': 'sword'},
-		'defend': {'pos' : vector(40, -20), 'icon': 'shield'},
-		'switch': {'pos' : vector(40, 20), 'icon': 'arrows'},
-		'catch':  {'pos' : vector(30, 60), 'icon': 'hand'}},
-	
-	'limited': {
-		'fight':  {'pos' : vector(30, -40), 'icon': 'sword'},
-		'defend': {'pos' : vector(40, 0), 'icon': 'shield'},
-		'switch': {'pos' : vector(30, 40), 'icon': 'arrows'}}
-}
+
+class BATTLE_POSITIONS(Enum):
+    class LEFT(Enum):
+        TOP = (360, 260)
+        CENTER = (190, 400)
+        BOTTOM = (410, 520)
+
+    class RIGHT(Enum):
+        TOP = (900, 260)
+        CENTER = (1110, 390)
+        BOTTOM = (900, 550)
+
+
+class BATTLE_LAYERS(Enum):
+    OUTLINE = auto()
+    NAME = auto()
+    MONSTER = auto()
+    EFFECTS = auto()
+    OVERLAY = auto()
+
+
+class BATTLE_CHOICES(Enum):
+    class FULL(Enum):
+        class FIGHT(Enum):
+            POS = vector(30, -60)
+            ICON = "sword"
+
+        class DEFEND(Enum):
+            POS = vector(40, -20)
+            ICON = "shield"
+
+        class SWITCH(Enum):
+            POS = vector(40, 20)
+            ICON = "arrows"
+
+        class CATCH(Enum):
+            POS = vector(30, 60)
+            ICON = "hand"
+
+    class LIMITED(Enum):
+        class FIGHT(Enum):
+            POS = vector(30, -40)
+            ICON = "sword"
+
+        class DEFEND(Enum):
+            POS = vector(40, 0)
+            ICON = "shield"
+
+        class SWITCH(Enum):
+            POS = vector(30, 40)
+            ICON = "arrows"
